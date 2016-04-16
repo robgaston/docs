bdce260a2449cc0df9a90b34d0627db0d14567fe
Rob Gaston <rgaston@fargeo.com>
2016-04-16 10:20:31 -0700

    setting up arches submodule for pulling md files from

--- custom/content.js
+++ custom/content.js
@@ -1,14 +1,10 @@
 var fs = require('fs');
 
-/**
- * This file exports the content of your website, as a bunch of concatenated
- * Markdown files. By doing this explicitly, you can control the order
- * of content without any level of abstraction.
- *
- * Using the brfs module, fs.readFileSync calls in this file are translated
- * into strings of those files' content before the file is delivered to a
- * browser: the content is read ahead-of-time and included in bundle.js.
- */
-module.exports =
-  '# Introduction\n' +
-  fs.readFileSync('./content/introduction.md', 'utf8') + '\n';
+var docPath = './arches/docbox/'
+var docs = fs.readdirSync(docPath);
+var content = '# Introduction\n#' + fs.readFileSync('./arches/README.md', 'utf8') + '\n';
+for (var i = 0; i < docs.length; i++) {
+    content += fs.readFileSync(docPath + docs[i], 'utf8') + '\n';
+}
+
+module.exports = content;
