008b1e6c5d875ea3c29f51d41b1fa8dc178c5d94
Rob Gaston <rgaston@fargeo.com>
2016-07-14 10:21:48 -0700

    copying img folder from arches/docbox

--- custom/get-content.js
+++ custom/get-content.js
@@ -1,10 +1,15 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var mustache = require('mustache');
 
 var contentTemplate = fs.readFileSync('./custom/content-template.js', 'utf8');
 var docs = fs.readdirSync('./arches/docbox/');
+var i = docs.indexOf('img');
+if (i !== -1) docs.splice(i, 1);
+
 var content = mustache.render(contentTemplate, {
     docs: docs
 });
 
 fs.writeFileSync('./custom/content.js', content);
+fs.removeSync('./img');
+fs.copySync('./arches/docbox/img/', './img');
